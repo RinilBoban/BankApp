36d3c24d1b529648d31c224f4613b00e3bb8a88b
RinilBoban <rinilboban15@gmail.com>
2022-11-13 10:55:50 +0530

    first angular project

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  acno:any
+  psw:any
+
+  testText='This is just to study String Interpolation'
+  data="Enter Account Number"
+
+  userDetails:any={
+    1000:{acno:1000, username:"Amal",password:123,balance:0},
+    1001:{acno:1001, username:"Anu",password:123,balance:0},
+    1002:{acno:1002, username:"Arun",password:123,balance:0},
+    1003:{acno:1003, username:"Mega",password:123,balance:0},
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    var acno=this.acno
+    var psw=this.psw
+    var userDetails=this.userDetails
+
+    if(acno in userDetails){
+      if(psw==userDetails[acno]['password']){
+        alert("Login Success")
+      }
+      else{
+        alert('Incorrect Password')
+      }
+    }
+    else{
+      alert('User not registered')
+    }
+
+
+    // alert("login clicked")
+  }
+
+  acnoChange(event:any){
+    this.acno=event.target.value
+    // console.log(event.target.value);
+  }
+  pswChange(event:any){
+    this.psw=event.target.value
+  }
+
+}
