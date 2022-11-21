1f1c408363fa9e3aae2217f4a46d3c66dacc681f
RinilBoban <rinilboban15@gmail.com>
2022-11-21 12:41:02 +0530

    Routing & Dependency Injection

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  acno:any
+  psw:any
+  amnt:any
+  acno1:any
+  psw1:any
+  amnt1:any
+  user:any
+
+  constructor(private ds:DataService) {
+
+    this.user=this.ds.currectuser
+    
+   }
 
   ngOnInit(): void {
+  }
+
+  deposit(){
+    var acno=this.acno
+    var psw=this.psw
+    var amnt=this.amnt
+    const result=this.ds.deposit(acno,psw,amnt)
+    if(result){
+      alert(`${amnt} is credited in your account and the available balance is ${result}`)
+    }
+
+    // alert('Deposit works')
+  }
+
+  withdraw(){
+    
+    var acno1=this.acno1
+    var psw1=this.psw1
+    var amnt1=this.amnt1
+    const result=this.ds.withdraw(acno1,psw1,amnt1)
+    if(result){
+      alert(`${amnt1} is debited from your account and the available balance is ${result}`)
+    }
+
+    // alert('Withdraw works')
+
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
+    currectuser:any
   // redundant data
     userDetails:any={
     1000:{acno:1000, username:"Amal",password:123,balance:0},
@@ -30,6 +31,9 @@ export class DataService {
   login(acno:any,psw:any){
 
     var userDetails=this.userDetails
+    this.currectuser=userDetails
+    this.currectuser=userDetails[acno]['username']
+
 
     if(acno in userDetails){
       if(psw==userDetails[acno]['password']){
@@ -44,9 +48,50 @@ export class DataService {
       alert('User not registered')
       return false
     }
-
-
     // alert("login clicked")
+  }
+
+  deposit(acno:any,psw:any,amnt:any){
+    let userDetails=this.userDetails
+    var amount=parseInt(amnt)   // to convert amount data type from string to int
+    if(acno in userDetails){
+      if(psw==userDetails[acno]['password']){
+        userDetails[acno]['balance']+=amount
+        return userDetails[acno]['balance']
+      }
+      else{
+        alert('Incorrect Password')
+        return false
+      }
+    }
+    else{
+      alert('Incorrect Username')
+      return false
+    }
+  }
+
+  withdraw(acno1:any,psw1:any,amnt1:any){
+    let userDetails=this.userDetails
+    var amount=parseInt(amnt1)
+    if(acno1 in userDetails){
+      if(psw1==userDetails[acno1]['password']){
+        if(amount<=userDetails[acno1]['balance']){
+          userDetails[acno1]['balance']-=amount
+          return userDetails[acno1]['balance']
+        }
+        else{
+          alert('Insufficient Balance')
+        }
+      }
+      else{
+        alert('Incorrect Password')
+        return false
+      }
+    }
+    else{
+      alert('Incorrect Username')
+      return false
+    }
   }
 
 }
