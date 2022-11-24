ab71fdb6396154d80072e95c35d742bcfa4c5c33
RinilBoban <rinilboban15@gmail.com>
2022-11-24 16:36:05 +0530

    Form Validation LoginForm

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -9,24 +10,29 @@ import { DataService } from '../services/data.service';
 })
 export class LoginComponent implements OnInit {
 
-  acno:any
-  psw:any
-
   testText='This is just to study String Interpolation'
   data="Enter Account Number"
 
-  constructor(private router:Router,private ds:DataService) { }
+  constructor(private router:Router,private ds:DataService,private fb:FormBuilder) { }
+
+  loginForm=this.fb.group({acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+                            psw:['',[Validators.required,Validators.pattern('[a-zA-Z0-9@#]+')]]})
 
   ngOnInit(): void {
   }
 
   login(){
-    var acno=this.acno
-    var psw=this.psw
-    const result=this.ds.login(acno,psw)
-    if(result){
-      alert("Login Success")
-      this.router.navigateByUrl('dashboard')
+    var acno=this.loginForm.value.acno
+    var psw=this.loginForm.value.psw
+    if(this.loginForm.valid){
+      const result=this.ds.login(acno,psw)
+      if(result){
+        alert("Login Success")
+        this.router.navigateByUrl('dashboard')
+      }  
+    }
+    else{
+      alert('Invalid Form')
     }
   }
 }
