c301b8d5219c8b4c7f1aa95d3a392809890a8784
RinilBoban <rinilboban15@gmail.com>
2022-12-04 16:17:52 +0530

    All forms validated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../services/data.service';
 
@@ -17,7 +18,19 @@ export class DashboardComponent implements OnInit {
   amnt1:any
   user:any
 
-  constructor(private ds:DataService, private router:Router) {
+  depositForm=this.fb.group({
+    acno:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    psw:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    amnt:['',[Validators.required,Validators.pattern('[0-9]+')]]
+  })
+  
+  withdrawForm=this.fb.group({
+    acno1:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    psw1:['',[Validators.required,Validators.pattern('[0-9]+')]],
+    amnt1:['',[Validators.required,Validators.pattern('[0-9]+')]]
+  })
+
+  constructor(private ds:DataService, private router:Router, private fb: FormBuilder) {
 
     this.user=this.ds.currectuser
     
@@ -27,9 +40,9 @@ export class DashboardComponent implements OnInit {
   }
 
   deposit(){
-    var acno=this.acno
-    var psw=this.psw
-    var amnt=this.amnt
+    var acno=this.depositForm.value.acno
+    var psw=this.depositForm.value.psw
+    var amnt=this.depositForm.value.amnt
     const result=this.ds.deposit(acno,psw,amnt)
     if(result){
       alert(`${amnt} is credited in your account and the available balance is ${result}`)
@@ -40,9 +53,9 @@ export class DashboardComponent implements OnInit {
 
   withdraw(){
     
-    var acno1=this.acno1
-    var psw1=this.psw1
-    var amnt1=this.amnt1
+    var acno1=this.withdrawForm.value.acno1
+    var psw1=this.withdrawForm.value.psw1
+    var amnt1=this.withdrawForm.value.amnt1
     const result=this.ds.withdraw(acno1,psw1,amnt1)
     if(result){
       alert(`${amnt1} is debited from your account and the available balance is ${result}`)
